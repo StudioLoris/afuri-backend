3db46b4ea759b35e26ee1aa827d32f4d422dab6f
Chenwil <willy.chen@hpe.com>
2018-06-02 15:25:32 +0800

    add koa and basic pm2 setup

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+import * as koa from 'koa';
+import * as Router from 'koa-router';
+
 import * as sourceMapSupport from 'source-map-support';
 sourceMapSupport.install(); // TODO: Only install it in DEV mode
-console.log('test');
+console.log('Afuri Backend Service');
+
+const app = new koa();
+const router = new Router();
+
+router.get('/', async (ctx) => {
+    ctx.body = 'Afuri Backend Service';
+});
+
+app.use(router.routes());
+
+app.listen(3001);
